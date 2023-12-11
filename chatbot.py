a47f4d8808a67eb15537afbce83313e59f60df31
Debbie <debbielow99@gmail.com>
2023-12-12 07:57:43 +0800

    first commit

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+import re
+from keras.models import load_model
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    message = request.args.get('message', 'Anonymous')
+
+    data = {'message': 'Hello from the API!'}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
