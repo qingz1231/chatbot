56a5c85a8ea456fc857df7ed48dc9c4e4c0f24a2
Debbie <debbielow99@gmail.com>
2023-12-12 15:40:28 +0800

    com

--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,19 @@
+
+
 from flask import Flask, jsonify, request
-import nltk
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-import pickle
-import numpy as np
-import re
-from keras.models import load_model
+# import nltk
+# from nltk.stem import WordNetLemmatizer
+# lemmatizer = WordNetLemmatizer()
+# import pickle
+# import numpy as np
+# import re
+# from keras.models import load_model
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/message', methods=['GET'])
 def get_data():
-    message = request.args.get('message', 'Anonymous')
+    # message = request.args.get('message', 'Anonymous')
 
     data = {'message': 'Hello from the API!'}
     return jsonify(data)
